c8786881eb870f2fadb920075027bfec1165d867
Cristian Mojica <cristiannmojica@gmail.com>
2026-01-20 21:45:38 -0500

    SETUP

--- app/app.py
+++ app/app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import (
+    Flask,
+    abort,
+    flash,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+    send_from_directory,
+)
+import os
 
 app = Flask(__name__)
 
+PUBLIC_REACT_DEV_SERVER = "http://localhost:5173"
+
+
+def serve_public_spa():
+
+    vite_path = request.path
+    # We want to go one folder up, to the parent
+    
+    vite_path = os.path.dirname(request.path)
+    if vite_path.startswith("/frontend"):
+        vite_path = vite_path.replace("/public-react", "") or "/"
+    vite_url = f"{PUBLIC_REACT_DEV_SERVER}{vite_path or '/'}"
+    return redirect(vite_url)
+
+
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    # Redirect the user to the Vite development server
+    return redirect("http://localhost:5173")
